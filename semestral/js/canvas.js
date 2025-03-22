46b53b52c593781b1ae16e10a3eb251171e66aad
Vojtěch Michal <vojtamichal04@gmail.com>
2025-03-22 20:51:47 +0100

    changes from cviceni 4

--- semestral/js/canvas.js
+++ semestral/js/canvas.js
@@ -13,9 +13,7 @@ class TileMap {
     constructor(ctx, tileSize) {
         this.ctx = ctx;
         this.tileSize = tileSize;
-        this.map = []; // will hold 2D array of tile values
-    
-    
+        this.map = [];
     }
 
     loadFromArray(array2D) {
@@ -77,4 +75,83 @@ canvas.addEventListener("click", function(event) {
         }
         tileMap.draw();
     }
+
+    // Create new enemy
+    let enemy = new Enemy(ctx, 0, 0, 1);
+    enemy.draw();
 });
+
+//přidat se Weather API 
+
+
+
+
+class Enemy{
+
+    constructor(ctx, x, y, speed){
+        this.ctx = ctx;
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = 64;
+        this.height = 64;
+    }
+
+    draw(){
+        this.ctx.fillStyle = "red";
+        for(let i = 0; i < 10; i++){
+            this.x += this.speed;
+            this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+
+
+
+}
+
+//weather API
+async function makeRequest(url, isJson = true) {
+    try {
+        const response = await fetch(url);
+        return isJson ? response.json() : response.text();  // vratime Promise
+    } catch (error) {
+        console.error(`Chyba při načítání ${url}:`, error);
+        return null;
+    }
+}
+
+async function getCountryAndWeather(countryName, cityName) {
+    const countryAPI = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,region,population,currencies`;
+    const weatherAPI = `https://wttr.in/${cityName}?format=3`;
+
+    try {
+        // We can use Promise.all() to fetch both APIs simultaneously and wait for both responses
+        const responses = await Promise.all([
+            makeRequest(countryAPI),
+            makeRequest(weatherAPI, false)
+        ]);
+
+        const countryData = responses[0];  // First API: REST Countries
+        const weatherData = responses[1];  // Second API: wttr.in
+
+        if (!countryData || !weatherData) {
+            throw new Error("Nepodařilo se načíst data.");
+        }
+
+        const country = countryData[0];
+        let formatted = `
+            ${weatherData}
+            ${country.name.common} (${country.capital[0]}) - ${country.region}
+            Population: ${country.population}
+            Currency: ${Object.values(country.currencies)[0].name}
+        `;
+
+        return formatted
+    } catch (error) {
+        console.error("Chyba při zpracování dat:", error);
+        return null;
+    }
+}
+
+ getCountryAndWeather("Czechia", "Prague").then(data => console.log(data));
